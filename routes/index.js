23909278643745071aa4cad3e5ba0a860c3a0eb6
njcushing <129661299+njcushing@users.noreply.github.com>
2023-10-31 21:16:09 +0000

    Add functionality for submitting message
    Submitting a message now appends the 'messages' array with a new
    object; the user and text properties of which are populated by the
    value of the Name and Message inputs, respectively, and creates a new
    Date object for the added property value. Also redirects the user to
    the '/' route so their new message can be viewed.

--- routes/index.js
+++ routes/index.js
@@ -14,9 +14,21 @@ const messages = [
     },
 ];
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
     res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+    res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+    messages.push({
+        text: req.body.message,
+        user: req.body.author,
+        added: new Date(),
+    });
+    res.redirect("/");
 });
 
 module.exports = router;
